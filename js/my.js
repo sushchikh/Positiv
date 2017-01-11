43a0a13cd3a8233e2287edd200bdfaa9f69c09ad
a.sushchikh <a.sushchikh@gmail.com>
2017-01-11 07:06:16 +0300

    Rebild CSS and JS files. Add paralax-effect.
    Added paralax effect on background and text.

--- js/my.js
+++ js/my.js
@@ -1,8 +1,21 @@
+$(document).ready(function(){
+
+
 $(window).scroll(function(){
 
 	var wScroll = $(this).scrollTop();
 
-	$('.first_paralax_block_text').css({
-		'transform' : 'translate -(0px, '+ wScroll /20 +'%)'
+	$('.first_paralax_block_background').css({
+		'transform' : 'translate(0px, -'+ wScroll/15 +'%)'
 	});
+
+	$('.first_paralax_block_text').css({
+		'transform' : 'translate(0px, '+ wScroll/1.5 +'%)'
+	});
+
+
+
+
+});
+
 });
