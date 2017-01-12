5e20dac00aa4d9ff6261637b5488e02d647e87cd
a.sushchikh <a.sushchikh@gmail.com>
2017-01-12 09:12:59 +0300

    Work at second pdralax block

--- js/my.js
+++ js/my.js
@@ -6,12 +6,18 @@ $(window).scroll(function(){
 	var wScroll = $(this).scrollTop();
 
 	$('.first_paralax_block_background').css({
-		'transform' : 'translate(0px, -'+ wScroll/15 +'%)'
+		'transform' : 'translate(0px, -'+ wScroll/13 +'%)'
 	});
 
 	$('.first_paralax_block_text').css({
-		'transform' : 'translate(0px, '+ wScroll/1.5 +'%)'
+		'transform' : 'translate(0px, '+ wScroll/1.7 +'%)'
 	});
+
+	$('.second_paralax_background').css({
+		'transform' : 'translate(0px, -'+ wScroll/13 +'%)'
+	});
+
+
 
 
 
